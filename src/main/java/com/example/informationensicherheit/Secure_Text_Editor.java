a65969872d83b57dba710563dfb1f75aa8b0a59e
Mohammadfarokh <mohammad_farokh95@proton.me>
2023-10-26 14:28:09 +0200

    26.10.2023

---
+++ src/main/java/com/example/informationensicherheit/Secure_Text_Editor.java
@@ -0,0 +1,93 @@
+package com.example.informationensicherheit;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.*;
+import java.util.Optional;
+
+public class Secure_Text_Editor extends Application {
+    private TextArea textArea;
+    private File currentFile;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Simple Text Editor");
+
+        textArea = new TextArea();
+        BorderPane layout = new BorderPane();
+        layout.setCenter(textArea);
+
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        MenuItem newMenuItem = new MenuItem("New");
+        MenuItem openMenuItem = new MenuItem("Open");
+        MenuItem saveMenuItem = new MenuItem("Save");
+        MenuItem exitMenuItem = new MenuItem("Exit");
+
+        fileMenu.getItems().addAll(newMenuItem, openMenuItem, saveMenuItem, exitMenuItem);
+        menuBar.getMenus().add(fileMenu);
+
+        newMenuItem.setOnAction(e -> newDocument());
+        openMenuItem.setOnAction(e -> openDocument());
+        saveMenuItem.setOnAction(e -> saveDocument());
+        exitMenuItem.setOnAction(e -> primaryStage.close());
+
+        layout.setTop(menuBar);
+
+        Scene scene = new Scene(layout, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void newDocument() {
+        textArea.clear();
+        currentFile = null;
+        //this is just a test
+    }
+
+    private void openDocument() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            currentFile = file;
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                StringBuilder content = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+                textArea.setText(content.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void saveDocument() {
+        if (currentFile == null) {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+            File file = fileChooser.showSaveDialog(null);
+            if (file != null) {
+                currentFile = file;
+            } else {
+                return;
+            }
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentFile))) {
+            writer.write(textArea.getText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
